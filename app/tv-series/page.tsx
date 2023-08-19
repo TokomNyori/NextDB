6ae66aca3aefcbd92050ba5a9726965c2aafb079
Tokom Nyori <tokom.nyori@gmail.com>
2023-08-20 01:20:50 +0530

    added tv-series route page

---
+++ app/tv-series/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/Navbar'
+import MainLayout from '@/components/mainLayout/MainLayout'
+import Image from 'next/image'
+
+export default function Movies() {
+  const page_name: string = 'tv-series';
+  return (
+    <main className="mt-4 pb-16">
+      <MainLayout page_name={page_name} />
+    </main>
+  )
+}
