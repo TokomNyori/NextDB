d6a22d660d30fa71b0f247b313844455a94424a4
Tokom Nyori <tokom.nyori@gmail.com>
2023-08-22 19:15:54 +0530

    Polished with anime section and trailer videos

--- components/mainLayout/MainLayoutTwo.tsx
+++ components/mainLayout/MainLayoutTwo.tsx
@@ -301,22 +301,20 @@ export default function MainLayoutTwo({ page_name }: { page_name: string }) {
                     <div>Anime</div>
                 </Link>
             </div>
-            <div className="flex justify-center items-center gap-2">
-                <div className={`sorting-nav my-6 ml-2 flex gap-2 items-center ${isSticky ? 'fixed-nav' : ''}`}>
-                    <h1>Sort:</h1>
-                    <select className='font-Nunito p-2 rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-[#ffffff33]'
-                        name="categories" id="" value={val} onChange={changeCategory}>
-                        <option value="current season">Current Season</option>
-                        <option value="upcoming season">Upcoming Season</option>
-                        <option value={`season winter ${currentYear}`}>Season: Winter</option>
-                        <option value={`season spring ${currentYear}`}>Season: Spring</option>
-                        <option value={`season summer ${currentYear}`}>Season: Summer</option>
-                        <option value={`season fall ${currentYear}`}>Season: Fall</option>
-                        <option value={`season winter ${currentYear + 1}`}>Season: Winter {currentYear + 1}</option>
-                        <option value="top anime">Top Anime</option>
-                        <option value="top manga">Top Manga</option>
-                    </select>
-                </div>
+            <div className={`sorting-nav my-6 ml-2 flex gap-2 items-center ${isSticky ? 'fixed-nav' : ''}`}>
+                <h1>Sort:</h1>
+                <select className='font-Nunito p-2 rounded-md text-sm focus:outline-none focus:ring-1 focus:ring-[#ffffff33]'
+                    name="categories" id="" value={val} onChange={changeCategory}>
+                    <option value="current season">Current Season</option>
+                    <option value="upcoming season">Upcoming Season</option>
+                    <option value={`season winter ${currentYear}`}>Season: Winter</option>
+                    <option value={`season spring ${currentYear}`}>Season: Spring</option>
+                    <option value={`season summer ${currentYear}`}>Season: Summer</option>
+                    <option value={`season fall ${currentYear}`}>Season: Fall</option>
+                    <option value={`season winter ${currentYear + 1}`}>Season: Winter {currentYear + 1}</option>
+                    <option value="top anime">Top Anime</option>
+                    <option value="top manga">Top Manga</option>
+                </select>
             </div>
             <div
                 className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-4 px-2 md:px-6 lg:px-12">
